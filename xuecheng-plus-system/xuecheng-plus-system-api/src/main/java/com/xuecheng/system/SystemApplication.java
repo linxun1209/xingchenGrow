6e1c07b4ebc780c2db0a749d1299d1f0d2db0e64
linxun1209 <3056726345@qq.com>
2024-07-13 15:33:50 +0800

    7.13 成功修改启动system模块时候读取不到nacos的端口 导致默认使用8080

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/SystemApplication.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/SystemApplication.java
@@ -1,6 +1,7 @@
 package com.xuecheng.system;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -17,7 +18,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class SystemApplication {
 
+
     public static void main(String[] args) {
+
+
         SpringApplication.run(SystemApplication.class,args);
     }
 }
